8c0e6d5b54a27047331da19cabd5d507a76a1d87
DasBarenz <42988309+DasBarenz@users.noreply.github.com>
2018-11-27 20:50:43 -0700

    a lot more work done..nothing functioning  yet

--- liri.js
+++ liri.js
@@ -1,11 +1,10 @@
+//NOTE: i left off with getting the function for the movie put into code...need to create functions for spotify and the band thing....data/results are not displaying righ tnow
+
+
 require("dotenv").config();
 var axios = require("axios");
+var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
-
-// //concert-this
-// // spotify-this-song
-// // movie-this
-// // do-what-it-says
 
 // //bands in town
 // //to run it .... node liri.js concert-this <artist/band name here>
@@ -20,7 +19,7 @@ for (var i = 2; i < nodeArgs.length; i++) {
     if (i > 2 && i < nodeArgs.length) {
         artist = artist + "+" + nodeArgs[i];
     } else {
-       artist += nodeArgs[i];
+        artist += nodeArgs[i];
     }
 }
 var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
@@ -44,7 +43,7 @@ for (var i = 2; i < nodeArgs.length; i++) {
     if (i > 2 && i < nodeArgs.length) {
         song = song + "+" + nodeArgs[i];
     } else {
-       song += nodeArgs[i];
+        song += nodeArgs[i];
     }
 }
 // // Artist(s)
@@ -57,37 +56,54 @@ spotify.search({ type: 'track', query: song }, function (err, response) {
     }
 
     console.log(response);
+
+    // output = space + "================= LIRI FOUND THIS FOR YOU...==================" + 
+    // space + "Song Name: " + "'" +songName.toUpperCase()+ "'" +
+    // space + "Album Name: " + data.tracks.items[0].album.name +
+    // space + "Artist Name: " + data.tracks.items[0].album.artists[0].name +	
+    // space + "URL: " + data.tracks.items[0].album.external_urls.spotify + "\n\n\n";
+    // console.log(output);
+
+    //     fs.appendFile("log.txt", output, function (err) {
+    //       if (err) throw err;
+    //       console.log('Saved!');
+    //     });		
+    // };
 });
 
 //movie this
 //to run it ...... node liri.js movie-this '<movie name here>'
 // Store all of the arguments in an array
-var nodeArgs = process.argv;
+function movieThis() {
+    var nodeArgs = process.argv;
 
-// Create an empty variable for holding the movie name
-var movieName = "";
+    // Create an empty variable for holding the movie name
+    var movieName = "";
 
-// Loop through all the words in the node argument
-// And do a little for-loop magic to handle the inclusion of "+"s
-for (var i = 2; i < nodeArgs.length; i++) {
-    if (i > 2 && i < nodeArgs.length) {
-        movieName = movieName + "+" + nodeArgs[i];
-    } else {
-        movieName += nodeArgs[i];
-    }
-}
+    // Loop through all the words in the node argument
+    // And do a little for-loop magic to handle the inclusion of "+"s
+    for (var i = 2; i < nodeArgs.length; i++) {
+        if (i > 2 && i < nodeArgs.length) {
+            movieName = movieName + "+" + nodeArgs[i];
+        } else {
+            movieName += nodeArgs[i];
+        }
+        if (!movie) {
+            movie = "ghost";
+        }
 
-// Then run a request with axios to the OMDB API with the movie specified
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
-// This line is just to help us debug against the actual URL.
-console.log(queryUrl);
+        // Then run a request with axios to the OMDB API with the movie specified
+        var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
-axios.get(queryUrl).then(
-    function (response) {
-        console.log("Release Year: " + response.data.Year);
-    }
-);
+        // This line is just to help us debug against the actual URL.
+        console.log(queryUrl);
+
+        axios.get(queryUrl).then(
+            function (response) {
+                console.log("Release Year: " + response.data.Year);
+            }
+        );
 
 // do what it says
 //to run in ............ node liri.js do-what-it-says
