7aea061a0974deda37f1ea91bd3d2bcd16b531bb
DasBarenz <42988309+DasBarenz@users.noreply.github.com>
2018-11-27 19:13:06 -0700

    more set up in place

--- liri.js
+++ liri.js
@@ -1,1 +1,19 @@
 require("dotenv").config();
+var axios = require("axios");
+
+
+//from NPM for spotify
+var Spotify = require('node-spotify-api');
+ 
+var spotify = new Spotify({
+  id: "504d00084d084a3a98b852898183da12",
+  secret: "2e110f5104424de197ff042669d9c049"
+});
+ 
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+ 
+console.log(data); 
+});
