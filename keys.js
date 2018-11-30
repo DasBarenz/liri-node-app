457276ddef3340e71b22cbfb0bacfced3d90fc35
DasBarenz <42988309+DasBarenz@users.noreply.github.com>
2018-11-29 20:58:08 -0700

    bands in town working with my key...haven't massaged results yet

--- keys.js
+++ keys.js
@@ -4,3 +4,7 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+exports.bands = {
+  id: "53c587ef2883cf6237115ae97c05b29b",
+};
--- liri.js
+++ liri.js
@@ -4,7 +4,12 @@
 require("dotenv").config();
 var axios = require("axios");
 var Spotify = require('node-spotify-api');
-var spotify = new Spotify(keys.spotify);
+var keys = require('./keys.js')
+// var spotifyKey = new Spotify(keys.spotify);
+var bandsKey = keys.bands;
+
+
+
 
 // //bands in town
 // //to run it .... node liri.js concert-this <artist/band name here>
@@ -22,88 +27,90 @@ for (var i = 2; i < nodeArgs.length; i++) {
         artist += nodeArgs[i];
     }
 }
-var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+var queryUrl = "https://rest.bandsintown.com/artists/" + "cher" + "/events?app_id=" + bandsKey;
 // //Name of the venue
 // // Venue location
 // // Date of the Event (use moment to format this as "MM/DD/YYYY")
 // // This line is just to help us debug against the actual URL.
 console.log(queryUrl);
 
-axios.get(queryUrl).then(
-    function (response) {
+axios.get(queryUrl).then(function(response) {
         console.log(response.data);
     }
 );
 
+
+
+
+
 // //spotify
 // //to run it..... node liri.js spotify-this-song '<song name here>'
-var nodeArgs = process.argv;
-var song = "";
-for (var i = 2; i < nodeArgs.length; i++) {
-    if (i > 2 && i < nodeArgs.length) {
-        song = song + "+" + nodeArgs[i];
-    } else {
-        song += nodeArgs[i];
-    }
-}
-// // Artist(s)
-// // The song's name
-// // A preview link of the song from Spotify
-// // The album that the song is from
-spotify.search({ type: 'track', query: song }, function (err, response) {
-    if (err) {
-        return console.log('Error occurred: ' + err);
-    }
+// var nodeArgs = process.argv;
+// var song = "";
+// for (var i = 2; i < nodeArgs.length; i++) {
+//     if (i > 2 && i < nodeArgs.length) {
+//         song = song + "+" + nodeArgs[i];
+//     } else {
+//         song += nodeArgs[i];
+//     }
+// }
+// // // Artist(s)
+// // // The song's name
+// // // A preview link of the song from Spotify
+// // // The album that the song is from
+// spotify.search({ type: 'track', query: song }, function(err, response) {
+//     if (err) {
+//         return console.log('Error occurred: ' + err);
+//     }
 
-    console.log(response);
+//     console.log(response);
 
-    // output = space + "================= LIRI FOUND THIS FOR YOU...==================" + 
-    // space + "Song Name: " + "'" +songName.toUpperCase()+ "'" +
-    // space + "Album Name: " + data.tracks.items[0].album.name +
-    // space + "Artist Name: " + data.tracks.items[0].album.artists[0].name +	
-    // space + "URL: " + data.tracks.items[0].album.external_urls.spotify + "\n\n\n";
-    // console.log(output);
+//     // output = space + "================= LIRI FOUND THIS FOR YOU...==================" + 
+//     // space + "Song Name: " + "'" +songName.toUpperCase()+ "'" +
+//     // space + "Album Name: " + data.tracks.items[0].album.name +
+//     // space + "Artist Name: " + data.tracks.items[0].album.artists[0].name +	
+//     // space + "URL: " + data.tracks.items[0].album.external_urls.spotify + "\n\n\n";
+//     // console.log(output);
 
-    //     fs.appendFile("log.txt", output, function (err) {
-    //       if (err) throw err;
-    //       console.log('Saved!');
-    //     });		
-    // };
-});
+//     //     fs.appendFile("log.txt", output, function (err) {
+//     //       if (err) throw err;
+//     //       console.log('Saved!');
+//     //     });		
+//     // };
+// });
 
-//movie this
-//to run it ...... node liri.js movie-this '<movie name here>'
-// Store all of the arguments in an array
-function movieThis() {
-    var nodeArgs = process.argv;
+// //movie this
+// //to run it ...... node liri.js movie-this '<movie name here>'
+// // Store all of the arguments in an array
+// function movieThis() {
+//     var nodeArgs = process.argv;
 
-    // Create an empty variable for holding the movie name
-    var movieName = "";
+//     // Create an empty variable for holding the movie name
+//     var movieName = "";
 
-    // Loop through all the words in the node argument
-    // And do a little for-loop magic to handle the inclusion of "+"s
-    for (var i = 2; i < nodeArgs.length; i++) {
-        if (i > 2 && i < nodeArgs.length) {
-            movieName = movieName + "+" + nodeArgs[i];
-        } else {
-            movieName += nodeArgs[i];
-        }
-        if (!movie) {
-            movie = "ghost";
-        }
+//     // Loop through all the words in the node argument
+//     // And do a little for-loop magic to handle the inclusion of "+"s
+//     for (var i = 2; i < nodeArgs.length; i++) {
+//         if (i > 2 && i < nodeArgs.length) {
+//             movieName = movieName + "+" + nodeArgs[i];
+//         } else {
+//             movieName += nodeArgs[i];
+//         }
+//         if (!movie) {
+//             movie = "ghost";
+//         }
 
 
-        // Then run a request with axios to the OMDB API with the movie specified
-        var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+//         // Then run a request with axios to the OMDB API with the movie specified
+//         var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
-        // This line is just to help us debug against the actual URL.
-        console.log(queryUrl);
+//         // This line is just to help us debug against the actual URL.
+//         console.log(queryUrl);
 
-        axios.get(queryUrl).then(
-            function (response) {
-                console.log("Release Year: " + response.data.Year);
-            }
-        );
+//         axios.get(queryUrl).then(function(response) {
+//                 console.log("Release Year: " + response.data.Year);
+//             }
+//         );
 
 // do what it says
 //to run in ............ node liri.js do-what-it-says
